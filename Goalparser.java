1c1ba17828478e0ea49b377453f2a2a9a61a60bd
Aditya Jambhale <jambhaleaditya91@gmail.com>
2024-01-10 20:03:19 +0530

    string

---
+++ Goalparser.java
@@ -0,0 +1,27 @@
+public class Goalparser {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder();
+       int i = 0;
+        String  s="G()()()(al)";
+        while(i<s.length())
+         if (s.charAt(i)=='G') {
+        sb.append("G");
+        i++;
+         }
+         else if(s.charAt(i)=='('&&s.charAt(i+1)==')')
+         {
+           sb.append("o");
+           i+=2;
+         }
+         else if(s.charAt(i)=='(' && s.substring(i,i+4).equals("(al)"))
+         {
+            sb.append("al");
+            i+=4;
+         }
+         else {
+            System.out.println("Invalid");
+         }
+         System.out.println(sb.toString());
+
+    }
+}
